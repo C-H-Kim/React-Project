0217dec4d7dbc8aecb5dcb570fb99143d106cab7
C-H-Kim <nick6253@naver.com>
2023-02-27 19:48:56 +0900

    [feat] Welcome 컴포넌트 분리
    - 컴포넌트 분리
    - Strict Mode 해제

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
-
-const Welcome = (props) => {
-  console.log(props);
-  return (
-    <p>
-      안녕하세요! {props.userAge}세 {props.userHeight}cm {props.userName}님!
-    </p>
-  );
-};
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Welcome userName="김창훈" userAge={26} userHeight={170}></Welcome>
+      <Welcome userName="홍길동" userAge={20} userHeight={180}></Welcome>
+      <Welcome userName="고길동" userAge={47} userHeight={176}></Welcome>
     </div>
   );
 }
