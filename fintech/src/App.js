4e9c0dee244fb9de410302f77abf32df4198fd35
C-H-Kim <nick6253@naver.com>
2023-02-28 00:12:49 +0900

    [feat] ListComponent 추가
    - 리스트 출력 실습용 파일 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Welcome from './components/Welcome';
 import InputComponent from './components/InputComponent';
+import ListComponents from './components/ListComponents';
 
 function App() {
   return (
     <div className="App">
       <Welcome userName="김창훈" userAge={26} userHeight={170}></Welcome>
       <InputComponent></InputComponent>
+      <ListComponents></ListComponents>
     </div>
   );
 }
