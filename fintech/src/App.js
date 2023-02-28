e933031d603f0f61d1dd2a6f48b64e493cb9f140
C-H-Kim <nick6253@naver.com>
2023-02-28 23:18:11 +0900

    [feat] Styled-Component module 추가 설치
    - 설치 명령어 : npm install styled-components

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Welcome from './components/Welcome';
 import InputComponent from './components/InputComponent';
 import ListComponents from './components/ListComponents';
+import StyledComponent from './components/StyledComponent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Welcome userName="김창훈" userAge={26} userHeight={170}></Welcome>
       <InputComponent></InputComponent>
       <ListComponents></ListComponents>
+      <StyledComponent></StyledComponent>
     </div>
   );
 }
