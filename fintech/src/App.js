08fdb77d94733400251203d50d0242a65aa3492b
C-H-Kim <nick6253@naver.com>
2023-03-01 14:23:38 +0900

    [feat] 뉴스 검색 페이지 작성을 위한 준비
    - AppHeader.js 파일 추가
    - App.js 라우터 경로 정리
    - NewsPage 추가
    - page, common 경로 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import InputComponent from "./components/InputComponent";
-import ListComponents from "./components/ListComponents";
-import AxiosComponent from "./components/AxiosComponent";
+import NewsPage from "./pages/NewsPage";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome />}></Route>
-        <Route path="/input" element={<InputComponent />}></Route>
-        <Route path="/list" element={<ListComponents />}></Route>
-        <Route path="axios" element={<AxiosComponent />}></Route>
+        <Route path="/news" element={<NewsPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
