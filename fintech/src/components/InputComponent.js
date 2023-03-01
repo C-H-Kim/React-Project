1e7ea8a3743d32eb9e10dee3e190b93904e2a9db
C-H-Kim <nick6253@naver.com>
2023-03-02 01:02:04 +0900

    [feat] 뉴스 검색 페이지 개선 사항
    - 정렬 기능 select 박스로 추가

--- fintech/src/components/InputComponent.js
+++ fintech/src/components/InputComponent.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const InputComponent = ({ handleChange, handleClick }) => {
+const InputComponent = ({ handleChange, handleClick, handleSelect, selectList }) => {
     return (
         <>
             <input onChange={handleChange}></input>
             <button onClick={handleClick}>입력</button>
+            <select onChange={handleSelect}>
+                {selectList.map((item, index) => {
+                    return <option key={index} value={item.value}>{item.key}</option>
+                })}
+            </select>
         </>
     );
 };
--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -7,6 +7,18 @@ import ListComponents from "../components/ListComponents";
 const NewsPage = () => {
     const [searchInputText, setSearchInputText] = useState("");
     const [newsList, setNewsList] = useState([]);
+    const [selected, setSelected] = useState("publishedAt");
+
+    const selectList = [
+        {key: "최신순", value: "publishedAt"},
+        {key: "인기순", value: "popularity"},
+        {key: "관련순", value: "relevancy"},
+    ];
+    
+    const handleSelectSort = (e) => {
+        const { value } = e.target;
+        setSelected(value);
+    };
 
     const handleSearchInputChange = (e) => {
         const { value } = e.target;
@@ -17,7 +29,8 @@ const NewsPage = () => {
     const handleSearchButtonClick = () => {
         console.log("서버로 요청을 보냅니다.");
         // 템플릿 리터럴을 통해 API URL 작성
-        const requestUrl = `https://newsapi.org/v2/everything?q=${searchInputText}&from=2023-02-01&sortBy=popularity&apiKey=9559bd697b9c4e7dab65cdff4bf7369a&language=ko`;
+        const requestUrl = `https://newsapi.org/v2/everything?q=${searchInputText}&from=2023-02-04&sortBy=${selected}&apiKey=9559bd697b9c4e7dab65cdff4bf7369a&language=ko`;
+        console.log(requestUrl);
 
         // axios를 통해서 news API에 요청 보내기
         axios.get(requestUrl).then(({ data }) => {
@@ -32,6 +45,8 @@ const NewsPage = () => {
             <InputComponent
                 handleChange={handleSearchInputChange}
                 handleClick={handleSearchButtonClick}
+                handleSelect={handleSelectSort}
+                selectList={selectList}
             ></InputComponent>
             <ListComponents newsList={newsList}></ListComponents>
         </div>
