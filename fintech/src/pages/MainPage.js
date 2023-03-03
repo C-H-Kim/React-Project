9a071235cd843b07f0d52ec96088e5d56da310dc
C-H-Kim <nick6253@naver.com>
2023-03-03 18:07:39 +0900

    [feat] 메인 화면 구성하기
    - 사용자 정보 조회 API에 데이터 요청
    - 요청 후 응답받은 데이터를 MainAccountCard를 통해 렌더링

---
+++ fintech/src/pages/MainPage.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AppHeader from "../components/Common/AppHeader";
+import MainAccountCard from "../components/Main/MainAccountCard";
+
+const MainPage = () => {
+    useEffect(() => {
+        console.log("시작하자마자 일을 시작합니다.");
+        getAccountList();
+    }, []);
+
+    const [accountList, setAccountList] = useState([]);
+
+    const getAccountList = () => {
+        const accessToken = localStorage.getItem("accessToken(3legged)");
+        const userSeqNo = localStorage.getItem("userSeqNo");
+
+        const option = {
+            method: "",
+            url: "/v2.0/user/me",
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            params: {
+                user_seq_no: userSeqNo,
+            },
+        };
+
+        axios(option).then(({ data }) => {
+            console.log(data.res_list);
+            setAccountList(data.res_list);
+        });
+    };
+
+    return (
+        <div>
+            <AppHeader title={"계좌 목록"}></AppHeader>
+            {accountList.map((account, index) => {
+                return (
+                    <MainAccountCard
+                        key={index}
+                        bankName={account.bank_name}
+                        fintechUseNo={account.fintech_use_no}
+                    ></MainAccountCard>
+                );
+            })}
+        </div>
+    );
+};
+
+export default MainPage;
